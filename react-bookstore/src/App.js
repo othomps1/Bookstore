fbbdb959bc3fc005f973caa94b0f2fa60830d18a
Owen Thompson <othomps1986@gmail.com>
2019-01-25 14:12:14 -0700

    Search is up

--- react-bookstore/src/App.js
+++ react-bookstore/src/App.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react'
+import Search from './components/Search'
+import './App.css'
 
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Search />
+      </main>
+    )
+  }
+}
+
+export default App
--- react-bookstore/src/components/Search.js
+++ react-bookstore/src/components/Search.js
@@ -1,1 +1,35 @@
+import React from 'react'
 
+export default class Search extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            book: []
+        }
+    }
+
+    findBook = (e) => {
+        e.preventDefault()
+        const searchInput = e.target.value
+        const { matchingBook } = this.props
+        this.setState({
+            ...this.state,
+            input: searchInput
+        })
+
+        matchingBook(searchInput)
+        console.log(this.state)
+    }
+
+
+    render() {
+        return (
+            <form>
+                <label htmlFor='Book'>Search:
+                    <input type='text' value={this.state.input} onChange={this.findBook}></input>
+                </label>
+            </form>
+        )
+    }
+}
